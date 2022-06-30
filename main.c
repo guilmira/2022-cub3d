ff837296b859be5eafc4addb611150c2156abdc1
Guillermo Mira Osuna <miraosuna@usal.es>
2022-06-30 19:25:37 +0200

    2

--- main.c
+++ main.c
@@ -6,11 +6,27 @@
 /*   By: guilmira <guilmira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/29 10:39:56 by guilmira          #+#    #+#             */
-/*   Updated: 2022/06/30 16:17:57 by guilmira         ###   ########.fr       */
+/*   Updated: 2022/06/30 19:25:11 by guilmira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "0includes/cube.h"
+
+void	draw_grid(t_prog *game)
+{
+	t_data image;
+
+	image.img = mlx_new_image(game->mlx, OX_WINDOW, OX_WINDOW);
+
+	image.addr = mlx_get_data_addr\
+	(image.img, &image.bits_per_pixel, &image.line_length, &image.endian);
+
+	for (int i = 500; i < 1915; i++)
+		my_mlx_pixel_put(&image, i, 501, get_opposite( trgb_translate(0, 150, 150, 0)));
+
+	mlx_put_image_to_window(game->mlx, game->mlx_window, image.img, 0, 0);
+}
+
 
 /** PURPOSE : Describe CUBE.
  * 1. Define structure of program. */
@@ -29,7 +45,7 @@ int	main(void)
 	/* --------------------------------------------------------------- */
 	init_game(game);
 
-	parser(game, argc, argv);
+	//parser(game, argc, argv);
 	//input_management();
 
 	init_window(game, window_dimensions);
@@ -38,17 +54,9 @@ int	main(void)
 	//trace_rays();
 	//create_images();
 	//detect_hooks();
-	t_data image;
 
-	image.img = mlx_new_image(game->mlx, OX_WINDOW, OX_WINDOW);
-
-	image.addr = mlx_get_data_addr\
-	(image.img, &image.bits_per_pixel, &image.line_length, &image.endian);
-
-	for (int i = 500; i < 1915; i++)
-		my_mlx_pixel_put(&image, i, 501, get_opposite( trgb_translate(0, 150, 150, 0)));
-
-	mlx_put_image_to_window(game->mlx, game->mlx_window, image.img, 0, 0);
+	draw_grid(game);
+	
 	/* --------------------------------------------------------------- */
 	hooks_and_loops(game);
 	clean_exit(game);
