48a0ae513e8eb6c13aed6c878d58257aa64ba057
Guillermo Mira Osuna <miraosuna@usal.es>
2022-08-20 13:29:51 +0200

    2

--- 0sources/8draw3D/3D_frame_exe.c
+++ 0sources/8draw3D/3D_frame_exe.c
@@ -6,7 +6,7 @@
 /*   By: guilmira <guilmira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 15:48:19 by guilmira          #+#    #+#             */
-/*   Updated: 2022/08/20 13:26:38 by guilmira         ###   ########.fr       */
+/*   Updated: 2022/08/20 13:29:38 by guilmira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,11 @@ void	put_frame3D(t_prog *game)
 {
 	(void) game;
 
-
+	int	i;
+	i = -1;
+	/* while (++i < 900)
+		solid_pixel(game->image[CUB_3D], 900, 600 + i, GREEN);
+		i = -1;
+	while (++i < 900)
+		solid_pixel(game->image[CUB_3D], 100, 600 + i, GREEN); */
 }
