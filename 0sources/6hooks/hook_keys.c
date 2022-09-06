95a2822f2558dc1e948a1aa3e5ca37633c45f039
Joan <Kiriamblu@gmail.com>
2022-09-06 13:07:35 +0200

    posss

--- 0sources/6hooks/hook_keys.c
+++ 0sources/6hooks/hook_keys.c
@@ -15,10 +15,10 @@
 /** PURPOSE : Correct minimap status and reset screen. */
 void	update_player_location(t_prog *game)
 {
-	game->pl.position_coor[0] = (double)(game->pl.position[0]) * \
+	game->pl.position_coor[0] = (double)(game->pl.position[0]) * 
 	game->map2D.pixel_per_block[0] + (game->map2D.pixel_per_block[0] / 2);
-	game->pl.position_coor[1] = (double)(game->pl.position[1]) * \
-	game->map2D.pixel_per_block[1] + (game->map2D.pixel_per_block[1] / 2);
+	game->pl.position_coor[1] = (double)(game->map2D.map_y - (game->pl.position[1])) * \
+	game->map2D.pixel_per_block[1] + (game->map2D.pixel_per_block[1] / 2) - 1;
 }
 
 /** PURPOSE : Is there os is there not a minimap. */
