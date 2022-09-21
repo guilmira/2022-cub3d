e89ec0313108c46b7e434df58c420df67f73d9c4
KiriamBlu <Kiriamblu@gmail.com>
2022-09-21 16:44:53 +0200

    checkpoint

--- sources/8draw3D/draw_first_layer.c
+++ sources/8draw3D/draw_first_layer.c
@@ -13,7 +13,7 @@
 #include "cube.h"
 
 /** PURPOSE : Horizontal line in given coordinate. */
-static void put_horizontal_line(double coordinate_y, double limit_x, int colour, t_prog *game)
+/*static void put_horizontal_line(double coordinate_y, double limit_x, int colour, t_prog *game)
 {
 	int i;
 	int coor_y;
@@ -26,7 +26,7 @@ static void put_horizontal_line(double coordinate_y, double limit_x, int colour,
 	coor_y = (int) coor(coordinate_y, game->w1.size[1]);
 	while (++i < limit_x)
 		solid_pixel(image, i, coor_y, colour);
-}
+}*/
 
 /*static void horizontal_line_mat(double coordinate_y, int limit_x, mlx_texture_t *sky_texture, t_prog *game)
 {
@@ -56,7 +56,7 @@ static void put_horizontal_line(double coordinate_y, double limit_x, int colour,
 }
 */
 /** PURPOSE : extend horizonal lines through the screen. */
-static void draw_horizon(double origin[], int size, int colour, t_prog *game)
+/*static void draw_horizon(double origin[], int size, int colour, t_prog *game)
 {
 	int i;
 	double coor_y;
@@ -67,7 +67,7 @@ static void draw_horizon(double origin[], int size, int colour, t_prog *game)
 		coor_y = origin[1] + i;
 		put_horizontal_line(coor_y, game->w1.size[0], colour, game);
 	}
-}
+}*/
 
 /*static void draw_sf_mat(double origin[], uint32_t size, mlx_texture_t *sky_texture, t_prog *game)
 {
@@ -99,16 +99,15 @@ mlx_texture_t *get_texture_stripe(mlx_texture_t *texture, int percentage, t_prog
 	xy[0] = pixel - 1;
 	xy[1] = 0;
 	hw[0] = 1;
-	hw[1] = texture->width;
-	printf("%d texture->width\n", texture->width);
+	hw[1] = texture->height;
 	aux = mlx_texture_area_to_image(game->mlx, texture, xy, hw);
 	ret = malloc(sizeof(mlx_texture_t));
-	ret->width = 1;
-	ret->height = texture->height;
+	ret->width = aux->width;
+	ret->height = aux->height;
 	ret->bytes_per_pixel = 4;
-	ret->pixels = malloc(sizeof(uint8_t) * texture->height * 4);
+	ret->pixels = malloc(sizeof(uint8_t) * hw[1] * 4);
 	count = -1;
-	while(++count < (texture->height))
+	while(++count < (hw[1] * 4))
 		ret->pixels[count] = aux->pixels[count];
 	mlx_delete_image(game->mlx, aux);
 	return(ret);
@@ -129,11 +128,11 @@ void	draw_first_layer(t_prog *game)
 	colour_floor = rgb_t_translate(151, 151, 151, 255);
 	colour_sky = rgb_t_translate(40, 40, 40, 255);
 	sky_texture = mlx_load_png("textures/landscape.png");
-	stripe = get_texture_stripe(sky_texture, 50, game);
-	draw_horizon(origin, game->w1.size[0] / 2, colour_sky, game);
+	stripe = get_texture_stripe(sky_texture, 30, game);
+	//draw_horizon(origin, game->w1.size[0] / 2, colour_sky, game);
 	origin[1] = game->w1.size[1] / 2;
-	mlx_draw_texture(game->image[CUB_3D], stripe, 200, game->w1.size[1]/2);
-	draw_horizon(origin, game->w1.size[0] / 2, colour_floor, game);
+	mlx_draw_texture(game->image[CUB_3D], stripe, 100, 100);
+	//draw_horizon(origin, game->w1.size[0] / 2, colour_floor, game);
 	mlx_delete_texture(sky_texture);
 	mlx_delete_texture(stripe);
 }
