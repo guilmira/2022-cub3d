f3ca31d808751e1f878f3322ceb08dda2f271d39
KiriamBlu <Kiriamblu@gmail.com>
2022-09-21 16:11:27 +0200

    algo raro

--- sources/8draw3D/draw_first_layer.c
+++ sources/8draw3D/draw_first_layer.c
@@ -82,6 +82,38 @@ static void draw_horizon(double origin[], int size, int colour, t_prog *game)
 	}
 }*/
 
+
+/*inputs percentage of the stripe desired and outputs the stripe in texture*/
+mlx_texture_t *get_texture_stripe(mlx_texture_t *texture, int percentage, t_prog *game)
+{
+	mlx_texture_t	*ret;
+	mlx_image_t		*aux;
+	uint32_t		xy[2];
+	uint32_t		hw[2];
+	uint32_t		count;
+	uint32_t 		pixel;
+
+	pixel = (percentage * texture->width) / 100;
+	if (pixel - 1 < 0)
+		pixel = 1;
+	xy[0] = pixel - 1;
+	xy[1] = 0;
+	hw[0] = 1;
+	hw[1] = texture->width;
+	printf("%d texture->width\n", texture->width);
+	aux = mlx_texture_area_to_image(game->mlx, texture, xy, hw);
+	ret = malloc(sizeof(mlx_texture_t));
+	ret->width = 1;
+	ret->height = texture->height;
+	ret->bytes_per_pixel = 4;
+	ret->pixels = malloc(sizeof(uint8_t) * texture->height * 4);
+	count = -1;
+	while(++count < (texture->height))
+		ret->pixels[count] = aux->pixels[count];
+	mlx_delete_image(game->mlx, aux);
+	return(ret);
+}
+
 /** PURPOSE : floor layer + horizon sky. 
  * 1. game->w1.size[1] / 2 represents the middlepoint at OY. */
 void	draw_first_layer(t_prog *game)
@@ -89,18 +121,19 @@ void	draw_first_layer(t_prog *game)
 	int colour_floor;
 	int colour_sky;
 	mlx_texture_t *sky_texture;	
+	mlx_texture_t *stripe;
 	double origin[D2];
 
 	origin[0] = 0;
 	origin[1] = 0;
 	colour_floor = rgb_t_translate(151, 151, 151, 255);
 	colour_sky = rgb_t_translate(40, 40, 40, 255);
-	sky_texture = mlx_load_png("textures/brick_normal.png");
+	sky_texture = mlx_load_png("textures/landscape.png");
+	stripe = get_texture_stripe(sky_texture, 50, game);
 	draw_horizon(origin, game->w1.size[0] / 2, colour_sky, game);
-	origin[1] = 300;
-	//draw_sf_mat(origin, sky_texture->height, sky_texture, game);
 	origin[1] = game->w1.size[1] / 2;
-	mlx_draw_texture(game->image[CUB_3D], sky_texture, 1, game->w1.size[1]/2);
+	mlx_draw_texture(game->image[CUB_3D], stripe, 200, game->w1.size[1]/2);
 	draw_horizon(origin, game->w1.size[0] / 2, colour_floor, game);
 	mlx_delete_texture(sky_texture);
+	mlx_delete_texture(stripe);
 }
